673e36abc55ac909d01657b07e08c864c3bfaf14
GauravRajpt <joygrajput@gmail.com>
2023-09-15 23:27:38 +0530

    add books to store backend

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT= 5555;
+
+export const mongodbURL= "mongodb://127.0.0.1:27017/";
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,61 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongodbURL } from "./config.js";
+import mongoose from "mongoose";
+import { bookmodel } from "./models/bookmodels.js";
 
 
-const app= express();
 
-app.get("/",(req,res)=>{
-    console.log(req);
-   return res.status(234).send("welcome to book store")
+mongoose
+  .connect(mongodbURL, {
+    dbName: "bookStore-menubar",
+  })
+  .then(() => {
+    console.log("connected to database");
+    app.listen(PORT, () => {
+        console.log(`server is running on port ${PORT}`);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+//middleware for parse json
+
+app.use(express.json())
+
+//route for save a new book
+
+app.post("/books",async (req,res)=>{
+try {
+    if(
+        !req.body.title || 
+        !req.body.author ||
+        !req.body.publishYear
+    )
+    {
+        res.status(400).send(
+            {message:"send all required feilds title, author, publishyear"}
+        )
+    }
+
+    const newBook={
+        title:req.body.title,
+        author:req.body.author,
+        publishYear:req.body.publishYear
+    }
+
+    const book=await bookmodel.create(newBook);
+    return res.status(201).send(book)
+} catch (error) {
+    console.log(error.message);
+    res.status(500).send({message:error.message})
+}
 })
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+app.get("/", (req, res) => {
+ 
+  return res.status(234).send("welcome to book store");
+});
+
+
