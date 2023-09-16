54b84bbb436f41d525b6fdbae7404bae386bca1b
GauravRajpt <joygrajput@gmail.com>
2023-09-16 22:32:13 +0530

    update book in databbase

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,36 @@ const app = express();
 
 app.use(express.json())
 
+//get a single book by id
+
+app.get("/books/:id",async (req,res)=>{
+  try {
+
+    const {id}= req.params;
+      const books= await bookmodel.findById(id);
+      res.status(200).send({
+        count:books.length,
+        books:books
+      })
+  } catch (error) {
+    res.status(500).send({message:error.message})
+  }
+})
+
+//get methode to get all the books from database
+
+app.get("/books",async (req,res)=>{
+  try {
+      const books= await bookmodel.find();
+      res.status(200).send({
+        count:books.length,
+        books:books
+      })
+  } catch (error) {
+    res.status(500).send({message:error.message})
+  }
+})
+
 //route for save a new book
 
 app.post("/books",async (req,res)=>{
@@ -51,6 +81,28 @@ try {
     console.log(error.message);
     res.status(500).send({message:error.message})
 }
+})
+
+//update the book
+
+app.put("/books/:id",async(req, res)=>{
+  const {id}= req.params;
+  if(
+    !req.body.title,
+    !req.body.author,
+    !req.body.publishYear
+){
+  res.status(404).send({message:"please send all details books, author, publishyear"})
+}
+
+  const result=await bookmodel.findByIdAndUpdate(id,req.body)
+
+  if(!result){
+    res.status(404).send({message:"book not found"})
+  }
+  else{
+    res.status(200).send({message:"suucsdjfd"})
+  }
 })
 
 app.get("/", (req, res) => {
